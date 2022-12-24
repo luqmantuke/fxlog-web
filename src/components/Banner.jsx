bb39ec8557e1539b00615cd62a3c9c6230dea24a
luqmantuke <lsuleiman2002@gmail.com>
2022-12-24 20:02:22 +0300

    why you should use fxlog"

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {Box, Image, Center,Container, Heading,Text} from '@chakra-ui/react'
+import {Box, Image, Center, Heading,Text} from '@chakra-ui/react'
 import brandLogo from '../assets/logo/logo 1.png'
 import {IoLogoInstagram,IoLogoTwitter} from 'react-icons/io'
 import {MdEmail} from 'react-icons/md'
 import banner from '../assets/screenshots/iosFrame/banner_transparent.png'
+import googlePlayStore from '../assets/images/download-google.svg'
 const Banner = () => {
   return (
     <Box  >
@@ -21,21 +22,29 @@ const Banner = () => {
       </Box>
     </Box>
    </header>
-<Center>
-   <Box   ml={6} mr={6}>
+
+   <Box  alignContent='center' alignItems='center' textAlign='center'   ml={6} mr={6}>
+
     <Heading as='h2' color='#FAEAB1'>
     The Smartest Way To Journal Your Trades And Achieve Profitability.
     </Heading>
-    <Text mt='6' color='#FAF8F1'>
+     <Text mt={{md:6,lg:6,sm:8,base:8 }}  color='#FAF8F1'>
     Mastering the Art of Journaling for Successful Forex Trading. Maximizing Your Trading Performance Through Journaling
     </Text>
-    <Image src={banner}/>
+    <Image  mt={{md:6,lg:6,sm:8,base:8 }}   src={banner}/>
+   <Center>
+    
+    <Image height='30px' my={{md:5,lg:8,sm:5,base:5}}  src={googlePlayStore}/>
+    </Center> 
+    
+    
+
   </Box>
 
 
 
 
-  </Center>
+  
 
   </Box>
 
--- src/components/HowToApp.jsx
+++ src/components/HowToApp.jsx
@@ -1,19 +1,55 @@
 import React from 'react'
-import {Box, Image, Center,Container,Text} from '@chakra-ui/react'
+import {Box, Image, Center,Container,Text,Heading} from '@chakra-ui/react'
 import addTrade from '../assets/screenshots/iosFrame/add trade.jpg'
 import listTrade from '../assets/screenshots/iosFrame/list_trades.jpg'
 import tradeDetails from '../assets/screenshots/iosFrame/trade_details.jpg'
 import quickAnalytics from '../assets/screenshots/iosFrame/quick_analytics.jpg'
 import generalAnalytics from '../assets/screenshots/iosFrame/trade_analytics.jpg'
 import report from '../assets/screenshots/iosFrame/trade_report.jpg'
+import test from '../assets/screenshots/iosFrame/test.jpg'
 const HowToApp = () => {
   return (
-    <Center> 
-       <Text color='white'>
+    <Box > 
+     <Center>
+        
+          <Heading as='h3' mb={{md:5,lg:8,sm:5,base:5}}  color='white'>
        Why you should use FxLog 
-       </Text>
+       </Heading>
+        </Center>
+
+   <Center>
     
-  </Center>
+     <Box ml={6} mr={6}  display='flex'  flexDirection={{md:'column',lg:'row',sm:'column',base:'column'}}  >
+{/* First Row/Column */}
+   
+    <Box   boxSize='200px'  pb={{md:0,lg:0,sm:60,base:60}} alignContent='center'  alignItems='center' justifyContent='center' justifyItems='center' >
+        <Image width='100%'  src={addTrade} />
+    </Box>
+    <Box boxSize='200px'  py={{md:0,lg:0,sm:60,base:60}}  alignContent='center'  alignItems='center' justifyContent='center' justifyItems='center' >
+        <Image width='100%' src={listTrade} />
+    </Box>
+    <Box boxSize='200px'  py={{md:0,lg:0,sm:60,base:60}} alignContent='center'  alignItems='center' justifyContent='center' justifyItems='center' >
+        <Image width='100%'  src={tradeDetails} />
+    </Box>
+    </Box>
+    </Center>
+     <Center>
+    
+     <Box ml={6} mr={6}  mt={{md:60,lg:60,sm:0,base:0}}  display='flex'  flexDirection={{md:'column',lg:'row',sm:'column',base:'column'}}  >
+{/* First Row/Column */}
+   
+    <Box  boxSize='200px'   py={{md:0,lg:0,sm:60,base:60}} alignContent='center'  alignItems='center' justifyContent='center' justifyItems='center' >
+        <Image width='100%'  src={quickAnalytics} />
+    </Box>
+    <Box boxSize='200px' py={{md:0,lg:0,sm:60,base:60}} alignContent='center'  alignItems='center' justifyContent='center' justifyItems='center' >
+        <Image width='100%'  src={generalAnalytics} />
+    </Box>
+    <Box boxSize='200px'  py={{md:0,lg:0,sm:60,base:60}} alignContent='center'  alignItems='center' justifyContent='center' justifyItems='center' >
+        <Image width='100%'  src={report} />
+    </Box>
+    </Box>
+    </Center>
+  </Box>
  
   )
 }
