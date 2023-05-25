b22925b73594bb4c603f04ed34471b237dcaac8b
Luqman Tuke <59395383+luqmantuke@users.noreply.github.com>
2023-05-25 08:09:16 +0300

    70%

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -6,8 +6,8 @@ const Action = () => {
   return (
     <Flex justifyContent='center'>
     
-    <Link href='https://play.google.com/store/apps/details?id=com.tukesolutions.fxlog' isExternal>   <Image height='50px' mt={60} mb={20}  src={googlePlayStore}/></Link>
-    <Link href='https://apps.apple.com/us/app/fxlog/id6447367184' isExternal>   <Image height='50px' mt={60} mb={20}  src={appleStore}/></Link>
+    <Link href='https://play.google.com/store/apps/details?id=com.tukesolutions.fxlog' isExternal>   <Image height='70px' mt={60} mb={20}  src={googlePlayStore}/></Link>
+    <Link href='https://apps.apple.com/us/app/fxlog/id6447367184' isExternal>   <Image height='70px' mt={60} mb={20}  src={appleStore}/></Link>
     </Flex> 
   )
 }
